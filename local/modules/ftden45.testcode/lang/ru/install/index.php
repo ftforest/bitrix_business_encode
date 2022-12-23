083fdccdb539f6d17b24c267b4b66a2e247efc28
Denis Kolosov <ftforest640@gmail.com>
2022-12-23 11:45:28 +0700

    correct .gitignore

---
+++ local/modules/ftden45.testcode/lang/ru/install/index.php
@@ -0,0 +1,28 @@
+<?
+/**
+ * Created by PhpStorm
+ * User: Sergey Pokoev
+ * www.pokoev.ru
+ * @ Академия 1С-Битрикс - 2015
+ * @ academy.1c-bitrix.ru
+ */
+$MESS["FTDEN45_TEST_CODE_MODULE_NAME"] = "Модуль Тестирования Кода";
+$MESS["FTDEN45_TEST_CODE_MODULE_DESC"] = "Модуль Тестирования Кода";
+$MESS["FTDEN45_TEST_CODE_PARTNER_NAME"] = "ftden45";
+$MESS["FTDEN45_TEST_CODE_PARTNER_URI"] = "http://ftden45.ru/";
+
+$MESS["FTDEN45_TEST_CODE_DENIED"] = "Доступ закрыт";
+$MESS["FTDEN45_TEST_CODE_READ_COMPONENT"] = "Доступ к компонентам";
+$MESS["FTDEN45_TEST_CODE_WRITE_SETTINGS"] = "Изменение настроек модуля";
+$MESS["FTDEN45_TEST_CODE_FULL"] = "Полный доступ";
+
+$MESS["FTDEN45_TEST_CODE_INSTALL_TITLE"] = "Установка модуля";
+$MESS["FTDEN45_TEST_CODE_INSTALL_ERROR_VERSION"] = "Версия главного модуля ниже 14. Не поддерживается технология D7, необходимая модулю. Пожалуйста обновите систему.";
+
+#работа с .settings.php
+$MESS["FTDEN45_TEST_CODE_INSTALL_COUNT"] = "Количество установок модуля: ";
+$MESS["FTDEN45_TEST_CODE_UNINSTALL_COUNT"] = "Количество удалений модуля: ";
+
+$MESS["FTDEN45_TEST_CODE_NO_CACHE"] = 'Внимание, на сайте выключено кеширование!<br>Возможно замедление в работе модуля.';
+#работа с .settings.php
+?>
---
+++ local/modules/ftden45.testcode/lang/ru/options.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: Sergey Pokoev
+ * www.pokoev.ru
+ * @ Академия 1С-Битрикс - 2015
+ * @ academy.1c-bitrix.ru
+ */
+
+$MESS["FTDEN45_TEST_CODE_TAB_SETTINGS"] = "Основные настройки";
+$MESS["FTDEN45_TEST_CODE_FIELD_TEXT_TITLE"] = "Поле типа текст";
+$MESS["FTDEN45_TEST_CODE_FIELD_LINE_TITLE"] = "Поле типа строка";
+$MESS["FTDEN45_TEST_CODE_FIELD_LIST_TITLE"] = "Поле типа список";
